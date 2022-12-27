c264cdba9974d9fb612bc6fcfe429da690abbf5e
Fahri Yusa Utama <fahriyusautama.com>
2022-12-27 11:21:32 +0700

    + controller Auth

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    public function index()
+    public function login()
     {
         $this->load->view('auth/login');
     }
