a3023dc026ef0f46f55ce39a26c6d0ae4694227e
Fahri Yusa Utama <fahriyusautama.com>
2022-12-27 08:46:54 +0700

    update

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -7,6 +7,7 @@
     <title>AdminLTE 3 | Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
+    <script src="https://kit.fontawesome.com/546b782367.js" crossorigin="anonymous"></script>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,133 +1,134 @@
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="assets/img/ico.png" alt="Peternakan Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Peternakan</span>
-    </a>
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+    <img src="assets/img/ico.png" alt="Peternakan Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Peternakan</span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">sg ndue</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="assets/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
+      <div class="info">
+        <a href="#" class="d-block">sg ndue</a>
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./assets/index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./assets/index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./assets/index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-          <!-- menu -->
-          <br>
-          <br>
-          <li class="nav-header">Anggota</li>
-          <li class="nav-item">
-            <a href="anggota" class="nav-link">
-              <i class="nav-icon far fa-user"></i>
-              <p>
-                Anggota
-              </p>
-            </a>
-          </li>
-          <li class="nav-header">Team</li>
-          <li class="nav-item">
-            <a href="team" class="nav-link">
-              <i class="nav-icon far fa-user"></i>
-              <p>
-                Team
-              </p>
-            </a>
-          </li>
-          <li class="nav-header">Pakan</li>
-          <li class="nav-item">
-            <a href="pakan" class="nav-link">
-              <i class="nav-icon far fa-drumstick"></i>
-              <p>
-                Pakan
-              </p>
-            </a>
-          </li>
-          <li class="nav-header">Telur</li>
-          <li class="nav-item">
-            <a href="telur" class="nav-link">
-              <i class="nav-icon far fa-egg"></i>
-              <p>
-                Telur
-              </p>
-            </a>
-          </li>
-          <li class="nav-header">Panen</li>
-          <li class="nav-item">
-            <a href="panen" class="nav-link">
-              <i class="nav-icon far fa-drumstick"></i>
-              <p>
-                Panen
-              </p>
-            </a>
-          </li>
-          <li class="nav-header">Jadwal</li>
-          <li class="nav-item">
-            <a href="jadwal" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt"></i>
-              <p>
-                Jadwal
-              </p>
-            </a>
-          </li>
-          <br>
-          <br>
-          <!-- ./menu -->
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline">
+      <div class="input-group" data-widget="sidebar-search">
+        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-sidebar">
+            <i class="fas fa-search fa-fw"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item menu-open">
+          <a href="#" class="nav-link active">
+            <i class="nav-icon fas fa-tachometer-alt"></i>
+            <p>
+              Dashboard
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./assets/index.html" class="nav-link active">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v1</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./assets/index2.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v2</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./assets/index3.html" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Dashboard v3</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+
+        <!-- menu -->
+        <br>
+        <br>
+        <li class="nav-header">Anggota</li>
+        <li class="nav-item">
+          <a href="anggota" class="nav-link">
+            <i class="fa-solid far nav-icon fa-user"></i>
+            <p>
+              Anggota
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">Team</li>
+        <li class="nav-item">
+          <a href="team" class="nav-link">
+          <i class="fa-solid fa-user-group"></i>
+          <i class="far"></i>
+            <p>
+              Team
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">Pakan</li>
+        <li class="nav-item">
+          <a href="pakan" class="nav-link">
+            <i class="nav-icon far fa-drumstick"></i>
+            <p>
+              Pakan
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">Telur</li>
+        <li class="nav-item">
+          <a href="telur" class="nav-link">
+            <i class="nav-icon far fa-egg"></i>
+            <p>
+              Telur
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">Panen</li>
+        <li class="nav-item">
+          <a href="panen" class="nav-link">
+            <i class="nav-icon far fa-drumstick"></i>
+            <p>
+              Panen
+            </p>
+          </a>
+        </li>
+        <li class="nav-header">Jadwal</li>
+        <li class="nav-item">
+          <a href="jadwal" class="nav-link">
+            <i class="nav-icon far fa-calendar-alt"></i>
+            <p>
+              Jadwal
+            </p>
+          </a>
+        </li>
+        <br>
+        <br>
+        <!-- ./menu -->
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
