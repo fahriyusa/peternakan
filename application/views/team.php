14bd005f475e2133e69a80c22c23f8ea43c3b482
Rendyrah <renrahdan@gmail.com>
2022-12-28 11:32:29 +0700

    view team

--- application/views/team.php
+++ application/views/team.php
@@ -26,7 +26,9 @@
 
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">DataTable with default features</h3>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
+                  Tambah Team
+                </button>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
@@ -40,15 +42,26 @@
                   </tr>
                 </thead>
                 <tbody>
-                    <?php $no = 1 ?>
-                    <?php foreach ($data as $row) :?>
-                  <tr>
-                    <td><?= $no++ ?></td>
+                    <?php $no=1 ?>
+                    <?php
+                    if(isset($join2table)>0){
+                    foreach($join2table as $row){
+                      ?>
+                  <tr>      
+                    <td><?= $no++?></td>
                     <td><?=$row->nama_team?></td>
-                    <td><?=$row->id_anggota?></td>
-                    <td>Aksi</td>
+                    <td><?=$row->nama_anggota?></td>
+                    <td>
+                    <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modal-edit">
+                    Edit
+                    </button>
+                    <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modal-hapus">
+                    Hapus
+                    </button>
+                    </td>
                   </tr>
-                  <?php endforeach; ?>
+                  <?php } ?>
+                  <?php } ?>
                 </tbody>
               </table>
             </div>
@@ -65,3 +78,95 @@
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+<!-- modal Tambah -->
+<div class="modal fade" id="modal-default">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Tambah Nama Team</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form  method="POST">
+            <div class="modal-body">
+              <label>Nama Team</label>
+             <input type="text" name="nama_team" class="form-control mb-2" placeholder="Nama Team">
+             <label>Nama Anggota</label>
+             <select name="nama_anggota" class="form-control">
+              <?php foreach ($datateam as $key ) : ?>
+              <option value="<?php echo $key->id_anggota?>"><?php echo $key->nama_anggota ?></option>
+              <?php endforeach ?>
+             </select>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+
+      <!-- modal edit -->
+      <div class="modal fade" id="modal-edit">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Edit Nama Team</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form method="POST">
+            <div class="modal-body">
+              <label>Nama Team</label>
+             <input type="text" name="nama_team" class="form-control mb-2" placeholder="Nama Team">
+             <label>Nama Anggota</label>
+             <select name="id_anggota" class="form-control">
+              <?php foreach ($datateam as $key ) : ?>
+              <option value="<?php echo $key->id_anggota?>"><?php echo $key->nama_anggota ?></option>
+              <?php endforeach ?>
+             </select>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+              <button type="button" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+
+            <!-- modal edit -->
+            <div class="modal fade" id="modal-hapus">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Hapus Nama Team</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form method="POST">
+            <div class="modal-body">
+              Apakah anda yakin akan menghapus data ini?
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+              <button type="button" class="btn btn-danger">Hapus</button>
+            </div>
+            </form>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
