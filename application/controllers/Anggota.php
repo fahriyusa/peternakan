63cce76bbc3bcc6a2f5dc71ba915b3e161733b0a
felixzaa <felixzafaimicaarsetyafna@gmail.com>
2022-12-28 13:00:32 +0700

    update anggota

--- application/controllers/Anggota.php
+++ application/controllers/Anggota.php
@@ -2,11 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Anggota extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();   
+        $this->load->model('M_Anggota');
+    }
 	public function index()
 	{
+        $data['title'] = 'Anggota';
+        $data['anggota'] = $this->M_Anggota->get_data('anggota')->result();
+
+        //menampilkan view
         $this->load->view('layout/header');
         $this->load->view('layout/sidebar');
-		$this->load->view('anggota');
+		$this->load->view('v_anggota', $data);
         $this->load->view('layout/footer');
 	}
 }
