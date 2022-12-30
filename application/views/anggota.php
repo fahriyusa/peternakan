74aeb846bed55fc6cd8651a42ce158b28cd24e12
Fahri Yusa Utama <fahriyusautama.com>
2022-12-30 08:49:17 +0700

    update e urung iso

--- application/views/anggota.php
+++ application/views/anggota.php
@@ -26,8 +26,8 @@
 
           <div class="card">
             <div class="card-header">
-              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
-                <i class="fa fa-add"></i></a>
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-tambah">
+                <i class="fa fa-add"></i> tambah</a>
               </button>
             </div>
             <!-- /.card-header -->
@@ -53,13 +53,13 @@
                     <td><?= $row->jabatan ?></td>
                     <td><?= $row->tanggal_gabung ?></td>
                     <td>
-                      <a class="btn btn-warning"
-                        href="<?= base_url('Anggota/update_anggota') ?>/<?= $row->id_anggota ?>"><i
-                          class="fa fa-pencil"></i></a>
+                      <a data-toggle="modal" data-target="#modal-edit<?= $row->id_anggota; ?>"
+                        class="btn btn-warning btn-circle" data-popup="tooltip" data-placement="top"
+                        title="Edit Data"><i class="fa fa-pencil"></i></a>
+
                       <a class="btn btn-danger" href="<?= base_url('Anggota/delete_anggota') ?>/<?= $row->id_anggota ?>"
                         onclick="return confirm('Apakah Anda ingin menghapus si : (<?= $row->nama_anggota ?>)');"><i
                           class="fa fa-trash"></i></a>
-
                     </td>
                   </tr>
                   <?php endforeach; ?>
@@ -80,9 +80,9 @@
 </div>
 <!-- /.content-wrapper -->
 
-<!-- MODAL -->
+<!-- MODAL TAMBAH-->
 
-<div class="modal fade" id="modal-default">
+<div class="modal fade" id="modal-tambah">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -117,8 +117,8 @@
           </div>
           <!-- /.card-body -->
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-            <button type="submit" class="btn btn-primary" id="tombolSimpan">Simpan</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-success" id="tombolSimpan">Simpan</button>
           </div>
       </form>
     </div>
@@ -127,5 +127,62 @@
   <!-- /.modal-content -->
 </div>
 <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<!-- MODAL EDIT -->
+<?php foreach ($data as $row) { ?>
+<div class="modal fade" id="modal-edit<?= $row->id_anggota ?>">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Edit Anggota</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <!-- form start -->
+      <form method="post" action="<?= base_url('Anggota/update_anggota') ?>">
+
+        <input type="hidden" name="id_anggota" id="id_anggota" value="<?= $row->id_anggota ?>" class="form-control">
+        <div class="card-body">
+          <div class="form-group">
+            <label for="nama_anggota">Nama Anggota</label>
+            <input type="nama" value="<?= $row->nama_anggota ?>" class="form-control" id="nama_anggota"
+              name="nama_anggota" placeholder="Nama Anggota">
+          </div>
+          <div class="form-group">
+            <label>Status</label>
+            <select value="<?= $row->status ?>" class="form-control" type="select" name="status">
+              <option value="">--Pilih--</option>
+              <option value="a" <?= $row->status == 'a' ? 'selected' : ''; ?>>Aktif</option>
+              <option value="na" <?= $row->status == 'na' ? 'selected' : ''; ?>>Non aktif</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="Jabatan">Jabatan</label>
+            <input type="text" value="<?= $row->jabatan ?>" class="form-control" id="jabatan" name="jabatan"
+              placeholder="Jabatan">
+          </div>
+          <div class="form-group">
+            <label for="exampleInpTanggal">Tanggal Gabung</label>
+            <input type="date" value="<?= $row->tanggal_gabung ?>" class="form-control" name="tanggal_gabung"
+              id="tanggal_gabung" placeholder="Tanggal Gabung">
+          </div>
+          <!-- /.card-body -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="reset" class="btn btn-warning">Riset</button>
+            <button type="submit" class="btn btn-success" id="tombolSimpan">Simpan</button>
+          </div>
+      </form>
+
+    </div>
+    <!-- /.card -->
+  </div>
+  <!-- /.modal-content -->
+</div>
+<!-- /.modal-dialog -->
+<?php } ?>
 </div>
 <!-- /.modal -->
