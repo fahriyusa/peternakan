ba8261c402d91d7b2c59f83560777ef4b00b958c
Fahri Yusa Utama <fahriyusautama.com>
2022-12-30 13:06:19 +0700

    update

--- application/views/jadwal.php
+++ application/views/jadwal.php
@@ -52,12 +52,8 @@
               </div>
               <div class="card-body">
                 <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
-                  <ul class="fc-color-picker" id="color-chooser">
+                  <ul class="fc-color-picker" id="color-chooser">``
                     <li><a class="text-primary" href="#"><i class="fas fa-square"></i></a></li>
-                    <li><a class="text-warning" href="#"><i class="fas fa-square"></i></a></li>
-                    <li><a class="text-success" href="#"><i class="fas fa-square"></i></a></li>
-                    <li><a class="text-danger" href="#"><i class="fas fa-square"></i></a></li>
-                    <li><a class="text-muted" href="#"><i class="fas fa-square"></i></a></li>
                   </ul>
                 </div>
                 <!-- /btn-group -->
