3e5654cc193fc1a90aa36bf91cccd5bd99448562
chanmukti6 <chanmukti73@gmail.com>
2023-01-03 14:26:08 +0700

    update pakan

--- application/controllers/Pakan.php
+++ application/controllers/Pakan.php
@@ -6,21 +6,11 @@ class Pakan extends CI_Controller {
     {
         parent::__construct();   
         $this->load->model('M_Pakan');
-
-        if ($this->session->userdata('authenticated') != true) {
-			redirect(base_url("auth"));
-		}
     }
 	public function index()
 	{
         //mengambil data
-<<<<<<< HEAD
-<<<<<<< HEAD
         $data['data'] = $this->db->get('data_pakan')->result();
-=======
-        $query = $this->M_Pakan->getPakan();
-        $data = array('data' => $query);
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
 
         //menampilkan view
         $this->load->view('layout/header');
@@ -28,55 +18,26 @@ class Pakan extends CI_Controller {
 		$this->load->view('pakan', $data);
         $this->load->view('layout/footer');
 	}
-<<<<<<< HEAD
     
     public function insert_pakan()
     {
         $team = $this->input->post('team');
         $tgl_produksi_pakan = $this->input->post('tgl_produksi_pakan');
         $jumlah = $this->input->post('jumlah');
-
         $data = array(
             'team' => $team,
             'tgl_produksi_pakan' => $tgl_produksi_pakan,
-            'jumlah' => $jumlah,
+            'jumlah' => $jumlah
         );
+        // var_dump($data);
         $this->db->insert('data_pakan', $data);
         redirect('pakan');
     }
+    //delete
+    public function delete_pakan($id)
+    {
+        $this->M_Pakan->delete_pakan($id);
+        redirect('Pakan');
+    }
    
-=======
-
-    public function ambil_pakan()
-	{
-        //mengambil data
-        $query = $this->M_Pakan->getambilPakan();
-=======
-        $query = $this->M_Pakan->getPakan();
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
-        $data = array('data' => $query);
-
-        //menampilkan view
-        $this->load->view('layout/header');
-        $this->load->view('layout/sidebar');
-		    $this->load->view('ambil_pakan', $data);
-        $this->load->view('layout/footer');
-	}
-<<<<<<< HEAD
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
-=======
-
-    public function ambil_pakan()
-	{
-        //mengambil data
-        $query = $this->M_Pakan->getambilPakan();
-        $data = array('data' => $query);
-
-        //menampilkan view
-        $this->load->view('layout/header');
-        $this->load->view('layout/sidebar');
-		    $this->load->view('ambil_pakan', $data);
-        $this->load->view('layout/footer');
-	}
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
 }
--- application/models/M_Pakan.php
+++ application/models/M_Pakan.php
@@ -4,48 +4,21 @@ defined('BASEPATH') or exit ('No Direct script access allowed');
 class M_Pakan extends CI_Model
 {
 
-    //get 
-
-    public function getPakan()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-    {
-        $query = $this->db->get('data_pakan');
-        return $query->result();
-    }
-    public function getambilPakan()
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
-    {
-        $query = $this->db->get('data_pakan');
-        return $query->result();
-    }
-    public function getambilPakan()
->>>>>>> ae6fd1621115c3419a222cefca3ad25fe09dc37d
-    {
-        $this->db->from('data_pakan');
-        $query = $this->db->get();
-        return $query->result();
-    }
-
-    public function getAmbilPakan()
-    {
-        $query = $this->db->get('ambil_pakan');
-        return $query->result();
-    }
+    
 
     //insert
     public function insert_pakan($data)
     {
         $this->db->insert('data_pakan',$data);
     }
-    
-    public function simpan_panen()
+    //delete
+    public function delete_pakan($id)
     {
-        $hasil = $this->db->query("INSERT INTO pakan (tanggal_produksi,jumlah') VALUE ('$tanggal','$jumlah')");
-        return $hasil;
+        $this->db->where('id', $id);
+        $this->db->delete('data_pakan');
     }
+    
+    
     
 
 
