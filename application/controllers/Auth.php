e114bbeefcf2404d19f180be7888f605e8e1fa84
Fahri Yusa Utama <fahriyusautama.com>
2023-01-13 10:27:06 +0700

    update pada alert login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,7 +6,7 @@ class Auth extends CI_Controller
     {
         parent::__construct();
         $this->load->model('M_auth');
-        
+
     }
 
     public function index()
@@ -26,81 +26,38 @@ class Auth extends CI_Controller
         // if ($this->form_validation->run() == FALSE) {
         //     return $this->load->view('auth/login');
         // }
-        
+
         $username = $this->input->post('username');
         $password = $this->input->post('password');
-        
         $user = $this->M_auth->selectuser($username);
-        
+
         if ($user) {
             if (password_verify($password, $user['password'])) {
                 $data = [
-                    'username'=> $user['username'],               
-                    'password'=> $user['password']
+                    'username' => $user['username'],
+                    'password' => $user['password']
                 ];
                 $this->session->set_userdata($data);
                 redirect('dashboard');
-            }else {
+            } else {
                 $this->session->set_flashdata(
-                    'message','<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>Password Salah</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                    aria-label="Close"></button>
-                    </div>'
+                    'message', '<div class=" salah">'
                 );
                 redirect('auth');
             }
-        }else {
+        } else {
             $this->session->set_flashdata(
-                'message','<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>Username Tidak Terdaftar</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert"
-                    aria-label="Close"></button>
-                    </div>'
+                'message', '<div class="tidakada">'
             );
             redirect('auth');
         }
-        // if ($this->M_auth->login($username,$password)) {
-        //     redirect('dashboard');
-        // }else {
-        //     $this->session->set_flashdata('message_login_error', 'Login Gagal');
-        // }
-
-        // $this->load->view('auth/login');
-        // cek username 
-        // if($cek->num_rows() > 0)
-        // {
-            // kita ambil isi dari record
-            // $hasil = $cek->row();
-            // if(password_verify($password, $hasil->password))
-            // {
-            //     // membuat session
-        //         $this->session->set_userdata('id_anggota', $hasil->id_anggota);
-        //         $this->session->set_userdata('is_login', TRUE);
-
-        //         // redirect ke admin
-        //         redirect(base_url('Dashboard'));
-        //     }else{
-
-        //         // jika password salah
-        //         $this->session->set_flashdata('failed','Password salah !');
-        //         redirect(base_url('Auth/login'));
-        //     }
-
-        // }else{
-
-        //     // jika username salah
-        //     $this->session->set_flashdata('failed','Username tidak Tersedia !');
-        //     redirect(base_url('Auth/login'));
-        // }
     }
 
-    
+    //logout
     public function logout()
     {
         $this->M_auth->logout();
         $this->session->sess_destroy();
         redirect(base_url('Auth'));
     }
-    
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php if ($this->session->flashdata('message_login_error')): ?>
+		<div class="invalid-feedback">
+			<?= $this->session->flashdata('message_login_error') ?>
+		</div>
+	<?php endif ?>
+	<?= $this->session->flashdata('message'); ?>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Peternakan | Log in</title>
-
 
 	<!-- Google Font: Source Sans Pro -->
 	<link rel="stylesheet"
@@ -16,57 +20,100 @@
 	<link rel="stylesheet" href="<?= base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css') ?>">
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+	<!-- SweetAlert2 -->
+	<link rel="stylesheet" href="<?= base_url('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') ?>">
+	<!-- Toastr -->
+	<link rel="stylesheet" href="<?= base_url('assets/plugins/toastr/toastr.min.css') ?>">
+
 </head>
 
 <body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-			<a><b>Peternakan</b></a>
+	<div class="login-box">
+		<div class="login-logo">
+			<a>Peternakan</a>
 		</div>
 		<!-- /.login-logo -->
-        <div class="card">
-            <div class="card-body login-card-body">
-            <p class="login-box-msg">Silahkan Diisi</p>
-				<?php if($this->session->flashdata('message_login_error')): ?>
-				<div class="invalid-feedback">
-					<?= $this->session->flashdata('message_login_error') ?>
-				</div>
-				<?php endif ?>
+		<div class="card">
+			<div class="card-body login-card-body">
+				<p class="login-box-msg">masukkan username dan password anda</p>
+				<form id="login" method="POST" action="<?= base_url('Auth/login') ?>">
+					<div class="input-group mb-3">
+						<input type="text" class="form-control" placeholder="username" id="username" name="username">
+						<div class="input-group-append">
+							<div class="input-group-text">
+								<span class="fas fa-envelope"></span>
+							</div>
+						</div>
+					</div>
+					<div class="input-group mb-3">
+						<input type="password" class="form-control" placeholder="Password" id="password"
+							name="password">
+						<div class="input-group-append">
+							<div class="input-group-text">
+								<span class="fas fa-lock"></span>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-8">
+							<div class="icheck-primary">
+								<input type="checkbox" id="remember">
+								<label for="remember">
+									Remember Me
+								</label>
+							</div>
+						</div>
+						<!-- /.col -->
+						<div class="col-4">
+							<button type="submit" class="btn btn-primary btn-block">Sign In</button>
+						</div>
+						<!-- /.col -->
+					</div>
+				</form>
 			</div>
-            
-            <?= $this->session->flashdata('message'); ?>
-			<form id="login" method="POST" action="<?= base_url('Auth/login') ?>" style="max-width: 600px;">
-				
-                <div class="input-group mb-3">
-					<input type="text" class="form-control" id="username" name="username" placeholder="Silahkan Diisi" required >
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
-                    </div>
-				</div>
+			<!-- /.login-card-body -->
+		</div>
+	</div>
+	<!-- /.login-box -->
 
-				<div class="input-group mb-3">
-					<input type="password" class="form-control" id="password" name="password" placeholder="Silahkan Diisi" required >
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-				</div>
-				<div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">Login</button>
-                </div>
-			</form>
-        </div>
-			<!-- /.login-box -->
+	<!-- jQuery -->
+	<!-- jQuery -->
+	<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+	<!-- AdminLTE App -->
+	<script src="<?= base_url('assets/dist/js/adminlte.min.js') ?>"></script>
+	<!-- jQuery -->
+	<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+	<!-- Bootstrap 4 -->
+	<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+	<!-- SweetAlert2 -->
+	<script src="<?= base_url('assets/plugins/sweetalert2/sweetalert2.min.js') ?>"></script>
+	<!-- Toastr -->
+	<script src="<?= base_url('assets/plugins/toastr/toastr.min.js') ?>"></script>
+	<script>
+		$(function () {
+			var Toast = Swal.mixin({
+				toast: true,
+				position: 'top-end',
+				showConfirmButton: false,
+				timer: 4000
 
-			<!-- jQuery -->
-			<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
-			<!-- Bootstrap 4 -->
-			<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
-			<!-- AdminLTE App -->
-			<script src="<?= base_url('assets/dist/js/adminlte.min.js') ?>"></script>
+				//yg kanggo
+			});
+			$('.berhasil').click(function () {
+				toastr.success('Login berhasil, Selamat Datang di Web Aplication Peternakan.')
+			});
+			$('.salah').click(function () {
+				toastr.error('Login Gagal, Username atau Password Salah')
+			});
+			$('.tidakada').click(function () {
+				toastr.error('Mohon Maaf, Username Anda Tidak Terdaftar')
+			});
+		});
+	</script>
 </body>
 
 </html>
